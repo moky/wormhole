3217d273363c85defcda16bc67e2be19df0e0481
Moky <albert.moky@gmail.com>
2021-12-05 13:50:46 +0800

    test fork

--- ipx-py/tests/shm.py
+++ ipx-py/tests/shm.py
@@ -1,33 +1,54 @@
 
+import multiprocessing
+import os
 from typing import List, Any
 
 from ipx import SharedMemoryCache
 
 
-shared = SharedMemoryCache(size=1024)
+g_shared = SharedMemoryCache(size=1024)
 
 
-def test_write(data: List[Any]):
-    print('-------- start writing')
-    print('shm: %s' % shared.shm[:])
+def test_write(shared: SharedMemoryCache, data: List[Any]):
+    print('======== start writing')
+    print('==== shm: %s' % shared.shm[:])
     for item in data:
-        print('---- write: %s' % item)
+        print('==== write: %s' % item)
         shared.put(item)
-        print('shm: %s' % shared.shm[:])
-    print('-------- stop writing')
+        print('==== shm: %s' % shared.shm[:])
+    print('======== stop writing')
 
 
-def test_read():
+def test_read(shared: SharedMemoryCache):
+    if shared is None:
+        shared = g_shared
     print('-------- start reading')
-    print('shm: %s' % shared.shm[:])
+    print('---- shm: %s' % shared.shm[:])
     data = shared.get()
     while data is not None:
         print('---- read: %s' % data)
-        print('shm: %s' % shared.shm[:])
+        print('---- shm: %s' % shared.shm[:])
         data = shared.get()
     print('-------- stop reading')
 
 
+def test_process():
+    son = multiprocessing.Process(target=test_read, args=(g_shared,))
+    son.start()
+    test_write(shared=g_shared, data=['Hello', 'world', 123])
+
+
+def test_fork():
+    print('******** Process (%d) start...' % os.getpid())
+    pid = os.fork()
+    if pid == 0:
+        print('---- Child process %d, parent=%d' % (os.getpid(), os.getppid()))
+        test_read(g_shared)
+    else:
+        print('==== Parent process %d, child=%d' % (os.getpid(), pid))
+        test_write(shared=g_shared, data=['Hello', 'world', 123])
+
+
 if __name__ == '__main__':
-    test_write(data=['Hello', 'world', 123])
-    test_read()
+    # test_process()
+    test_fork()
